513ac2f14aa2a0108f5cb540e0003e7a871b057a
selimreza777 <selimrezacse@gmail.com>
2025-08-08 20:58:26 +0600

    ascending

--- sort.js
+++ sort.js
@@ -1,4 +1,17 @@
 const numbers = [5, 1, 4, 3, 2, 6];
-console.log(numbers);
+// console.log(numbers);
 numbers.sort();
-console.log(numbers);
+// console.log(numbers);
+
+const friends = ['akib', 'bamun', 'dhoyon', 'eamil', 'colash'];
+friends.sort();
+// console.log(friends);
+
+const ages = [11, 100, 15, 25, 105, 205, 300];
+// console.log(ages);
+ages.sort();
+// console.log(ages);
+
+// ascending(choto theke boro dike jacce)
+const sorted_ages = ages.sort(function (a, b) { return a - b });
+console.log(sorted_ages);
